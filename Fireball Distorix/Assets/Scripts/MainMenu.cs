209d12d50420803404e47a478c5a4b04c349feca
Xerro <xerro24@gmail.com>
2022-03-18 13:43:47 -0700

    0.3.8
    Added a temp level 4, 5, 6, 7 and level select

--- Fireball Distorix/Assets/Scripts/MainMenu.cs
+++ Fireball Distorix/Assets/Scripts/MainMenu.cs
@@ -30,4 +30,39 @@ public class MainMenu : MonoBehaviour
         print("Goodbye >~<");
     }
 
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Level6()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Level7()
+    {
+        SceneManager.LoadScene(7);
+    }
+
 }
